751598719f481c91c6fe7564b77e4b0c74e21463
muhammadakif07 <53373776+muhammadakif07@users.noreply.github.com>
2019-08-01 00:07:15 +0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -23,12 +23,31 @@ function add(a, b, c) {
 bolo("hello world");
 
 
+
+function add(t, u) {
+    var sum = t + u;
+    return sum;
+
+}
+
+bolo(add(t, u));
+
+
+
+
+
+
 var name = "ali";
 
 var a = 12;
 var b = 13;
 var x = 14;
 var y = 11;
+var t = 6;
+var u = 13;
+var name = "faraz";
+bolo(name);
+bolo(add(a, b, t));
 
 
 bolo(name);
@@ -54,3 +73,100 @@ bolo(f2c(44));
 bolo(c2f(45));
 
 bolo("hi");
+
+var name = "akif";
+bolo(name);
+function multiply(t,u){
+    var product = t * u;
+    return product;
+
+}
+
+
+bolo(multiply(t,u));
+
+bolo (add(t,u));
+function add(a, b, t){
+    var sum = a + b + t;
+    return sum;
+
+}
+bolo (add(a,b,t));
+function add(x, y,u){
+    var sum = x + y + u;
+    return sum;
+
+}
+
+bolo (add(x,y,u));
+function add(a,b,x,y,t,u){
+    var sum = a + b + x + y + t + u;
+    return sum;
+
+}
+bolo (add(a,b,x,y,t,u));
+
+var name = "majid";
+var name = "areeb";
+var name = "muteeb";
+var name = "anif";
+var name = "naseem";
+bolo (name);
+
+
+function c2k(celsius){
+    var kelvin = celsius + 273.15;
+    return kelvin;
+
+}
+bolo (c2k(3));
+
+function k2c(kelvin){
+    var celsius = kelvin - 273.15;
+    return celsius;
+
+}
+
+
+bolo (k2c(3));
+
+var name = "sohail";
+bolo (name);
+
+function add(a, b, x, y, t, u){
+    var sum = a + b + x + y + t + u;
+    return sum;
+
+}
+bolo (add(a,b,x,y,t,u));
+
+
+function kg2lbs(lbs){
+    var kg = lbs * 2.205;
+    return kg;
+
+}
+
+bolo (kg2lbs(2));
+
+function lbs2kg(kg){
+    var lbs = kg / 2.205
+    return lbs;
+
+}
+bolo (lbs2kg(2));
+
+
+function m2f(feet){
+    var meeters = feet * 3.281;
+    return meeters;
+
+}
+
+bolo (m2f(2));
+function f2m(meeters){
+    var feet = meeters / 3.281;
+    return feet;
+
+}
+bolo (f2m(2));
